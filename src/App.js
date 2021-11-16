218da3acf96fee3abac21593c900f916d845f697
LeonidMakhov <makhov-leonid999@yandex.ru>
2021-11-16 14:20:19 +0300

    new math  +/-

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import {useState} from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
-  const plus = () => {
-      setCounter(counter + 1)
-    }
-  const minus = () => {
-      setCounter(counter - 1)
-    }
+  // const plus = () => {
+  //     setCounter(counter + 1)
+  //   }
+  // const minus = () => {
+  //     setCounter(counter - 1)
+  //   }
+  const change = (sign) => {
+        //sign === '+' ? setCounter(counter + 1) : setCounter(counter - 1)
+    setCounter(counter + sign);
+  }
 
   return (
     <div>
-      <button onClick={plus}>+ Plus</button>
+      {/*<button onClick={plus}>+ Plus</button>*/}
+      {/*  {counter}*/}
+      {/*<button onClick={minus}>Minus -</button>*/}
+
+      {/*<button onClick={() => change ('+')}>+ Plus</button>*/}
+      {/*  {counter}*/}
+      {/*<button onClick={() => change ('-')}>Minus -</button>*/}
+
+      <button onClick={() => change (1)}>+ Plus</button>
         {counter}
-      <button onClick={minus}>Minus -</button>
+      <button onClick={() => change (-1)}>Minus -</button>
+
+
+
+
     </div>
   );
 }
