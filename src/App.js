672daef15cef7fe03b99821aca6f96d76ca5d342
LeonidMakhov <makhov-leonid999@yandex.ru>
2021-11-16 14:03:39 +0300

    create +,-

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const plus = () => {
+      setCounter(counter + 1)
+    }
+  const minus = () => {
+      setCounter(counter - 1)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={plus}>+ Plus</button>
+        {counter}
+      <button onClick={minus}>Minus -</button>
     </div>
   );
 }
